c4d5c8eb5d15e4d1d231682ab1f5d95773709211
Nakul Sabharwal <nasabhar@microsoft.com>
2018-08-13 15:22:01 +0530

    first commit

---
+++ src/main/java/DemoPublish.java
@@ -0,0 +1,12 @@
+
+public class DemoPublish {
+	public int addTwoNumbers(int num1, int num2) {
+		System.out.println("Adding Two Numbers");
+		return num1+num2;
+	}
+	
+	public int multiplyTwoNumbers(int num1, int num2) {
+		System.out.println("Multiplying Two Numbers");
+		return num1*num2;
+	}
+}
---
+++ src/main/java/UsingDemoPublish.java
@@ -0,0 +1,7 @@
+
+public class UsingDemoPublish {
+	public static void main(String[] args) {
+		DemoPublish demoPublish = new DemoPublish();
+		System.out.println(demoPublish.addTwoNumbers(1, 2));
+	}
+}
